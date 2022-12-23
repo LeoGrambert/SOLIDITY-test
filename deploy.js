498a257bdedc6228885f48898515b6eac40fa9f4
Léo Grambert <leogrambert@protonmail.com>
2022-12-23 21:46:15 +0100

    feat: add deploy

--- deploy.js
+++ deploy.js
@@ -1,1 +1,26 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+
+const { abi, evm } = require("./compile");
+
+provider = new HDWalletProvider(
+  "custom parrot work know food nice fix screen solid noodle shrug divorce", //test key, no money here ;)
+  "https://goerli.infura.io/v3/9d221446523849d7805a16d5aaed57ca"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
